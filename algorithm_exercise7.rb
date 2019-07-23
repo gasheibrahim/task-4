4e0f6f2ebef3a5960618bd0327c4215c0e1be9a2
unknown <ibrahimgashema154@gmail.com>
2019-07-23 10:09:49 +0200

    second chance

--- algorithm_exercise7.rb
+++ algorithm_exercise7.rb
@@ -31,31 +31,23 @@ end
 			elsif(result==1)
 				return "you are Loss"
 			else
-				return "you are Draw" 
+				puts "you are Draw"
+				load("algorithm_exercise7.rb") 
 			end
-
 		end
-	 end
-
-while(true) do
-	puts "Welcome Press Any Number If You Want To Continue The Game Or Press 0 If You Want To Quit"
-	str=gets.to_i
-	if(str==0)
-		break
-	end
 puts "please select one among these following:"
 puts "0: Goo"
 puts "1: Choki"
 puts "2: Par"
 number=gets.to_i
 if(number<0 || number>3)
-	puts "out of boundary"
+	puts "please press number between 0 and 2"
 else
 	player = Player.new(number)
 	arr=[0,1,2]
 	randomly=arr[rand(arr.count)];
 	enemy = Enemy.new(randomly)
 	janken = Janken.new(player,enemy)
-	puts " it is a #{janken.pon(player.hand, enemy.hand)}"
+	puts "#{janken.pon(player.hand, enemy.hand)}"
 end
 end
